b57bc920abf1c3310dae2bbbdddb08f3b0a9ed36
cyberlancer <cyberlancer@gmail.com>
2023-05-06 09:11:05 +0800

    第一次提交

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CyTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
